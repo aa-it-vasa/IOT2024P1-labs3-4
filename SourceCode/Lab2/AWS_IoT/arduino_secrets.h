c737b72da4fc3e054c623f13fee2fcfb370dc18f
seb-ho <sebastian.horsch@abo.fi>
2024-10-26 15:57:58 +0300

    Update arduino_secrets.h

--- SourceCode/Lab2/AWS_IoT/arduino_secrets.h
+++ SourceCode/Lab2/AWS_IoT/arduino_secrets.h
@@ -1,4 +1,4 @@
-#define SECRET_SSID ""
-#define SECRET_PASS ""
-#define SECRET_BROKER  ""
-#define SECRET_GROUP  ""
+#define SECRET_SSID "BUFFALO2"
+#define SECRET_PASS "pleaseletmein"
+#define SECRET_BROKER  "akprfq7xi5a1p-ats.iot.eu-central-1.amazonaws.com"
+#define SECRET_GROUP  "TurkuGroupD"
